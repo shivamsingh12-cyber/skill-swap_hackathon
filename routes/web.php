e067113616c1a16c75a7f2910b5ba1ffdc212ded
Shivam_Singh <shivamsingh6490@gmail.com>
2025-07-12 17:36:11 +0530

    Modification in routes

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 
  Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login'  );
- Route::get('/loginverification', [LoginController::class, 'login'])->name('loginverification'  );
+ Route::post('/loginverification', [LoginController::class, 'login'])->name('loginverification'  );
  Route::any('/', [HomeController::class, 'showDashboard'])->name('/');
 Route::get('/homepage', [HomeController::class, 'showHomePage'])->name('homepage');
  Route::any('/forgot-password', [LoginController::class, 'forgotPassword'])->name('forgot.password');
